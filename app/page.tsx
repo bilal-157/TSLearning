d9e50f6e0a190d42e1ae766bccaf53bc843f39f6
Bilal Rafique <rafiqueb087@gmail.com>
2025-01-09 12:32:12 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  
+  let name:string = "bilal";
+  name = "billa";
+
+  console.log(name)
+  return (
+
+    <div>
+      <h1 className='bg-black text-white text-center'>THIS IS THE PRACTICE OF TYPESCRIPT BRO !</h1>
+    </div>
+  )
+}
+
+export default page
