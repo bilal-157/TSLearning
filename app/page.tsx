4c5929e5f10719cc7105794a205acbce3e5fda0e
Bilal Rafique <rafiqueb087@gmail.com>
2025-01-16 20:55:08 -0800

    The graphql route is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,52 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { ApolloProvider, gql, useQuery } from '@apollo/client';
+import client from '@/app/lib/appoloClient';
 
-const page = () => {
-  
-  type objStructure = {
-     
-    name:string,
-    age:number,
-    class:string
-  }
-
-  const data:objStructure = {
-    name:"bilal",
-    age:23,
-    class:"bscs"
-  } 
-  console.log(data)
-  return (
-
-    <div>
-      <h1 className='bg-black text-white text-center'>THIS IS THE PRACTICE OF TYPESCRIPT BRO !</h1>
-    </div>
-  )
+// Define the TypeScript types for the GraphQL query response
+interface Product {
+  email: string;
+  password: string;
 }
 
-export default page
+interface GetProductsData {
+  products: Product[];
+}
+
+// Define the GraphQL query
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      email
+      password
+    }
+  }
+`;
+
+const Home: React.FC = () => {
+  const { loading, error, data } = useQuery<GetProductsData>(GET_PRODUCTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {data?.products.map((product, index) => (
+          <li key={index}>
+            Email: {product.email} - Password: {product.password}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const App: React.FC = () => (
+  <ApolloProvider client={client}>
+    <Home />
+  </ApolloProvider>
+);
+
+export default App;
